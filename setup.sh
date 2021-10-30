80ae25274f0c21c3eadcd267a48d795dec6878e8
skarre-r <pls.never@pm.me>
2021-10-31 01:30:38 +0200

    refactor: update setup.sh to use built-in environment variables

--- setup.sh
+++ setup.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env zsh
 
-current_dir=$(pwd)
-user=$(whoami)
-user_dir="/Users/$user"
-
-if [[ -f "$user_dir/.zshrc" ]]; then
-    echo "$user_dir/.zshrc already exists"
+if [[ -f "$HOME/.zshrc" ]]; then
+    echo "$HOME/.zshrc already exists"
 else
-    ln -s "$current_dir/.zshrc" "$user_dir/.zshrc"
-    echo ".zshrc symlink created: $current_dir/.zshrc > $user_dir/.zshrc"
+    ln -s "$PWD/.zshrc" "$HOME/.zshrc"
+    echo ".zshrc symlink created: $PWD/.zshrc > $HOME/.zshrc"
 fi
+echo
 
-if [[ -f "$user_dir/.bash_profile" ]]; then
-    echo "$user_dir/.bash_profile already exists"
+if [[ -f "$HOME/.bash_profile" ]]; then
+    echo "$HOME/.bash_profile already exists"
 else
-    ln -s "$current_dir/.bash_profile" "$user_dir/.bash_profile"
-    echo ".bash_profile symlink created: $current_dir/.bash_profile > $user_dir/.bash_profile"
+    ln -s "$PWD/.bash_profile" "$HOME/.bash_profile"
+    echo ".bash_profile symlink created: $PWD/.bash_profile > $HOME/.bash_profile"
 fi
-
-
 
 exit 0
