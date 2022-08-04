153a5754f7a6604516773c4dbd6921622b1aa9af
skarre-r <pls.never@pm.me>
2022-08-04 23:12:32 +0200

    refactor(setup.sh): rework the script to symlink all dotfiles dynamically

--- setup.sh
+++ setup.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env zsh
 
-if [[ -f "$HOME/.zshrc" ]]; then
-    echo "$HOME/.zshrc already exists"
-else
-    ln -s "$PWD/.zshrc" "$HOME/.zshrc"
-    echo ".zshrc symlink created: $PWD/.zshrc > $HOME/.zshrc"
-fi
-echo
+# https://stackoverflow.com/a/20434740
+dotfiles_directory="$( cd "$( dirname "$0" )" && pwd )"
 
-if [[ -f "$HOME/.bash_profile" ]]; then
-    echo "$HOME/.bash_profile already exists"
-else
-    ln -s "$PWD/.bash_profile" "$HOME/.bash_profile"
-    echo ".bash_profile symlink created: $PWD/.bash_profile > $HOME/.bash_profile"
-fi
+
+symlink_dotfile() {
+    dotfile=$1
+    if [[ -f "$HOME/$dotfile" ]]; then
+        echo "WARNING: $HOME/$dotfile already exists"
+    else
+        ln -s "$dotfiles_directory/$dotfile" "$HOME/$dotfile"
+        echo "INFO: $dotfile symlink created ($dotfiles_directory/$dotfile >>> $HOME/$dotfile)"
+    fi
+}
+
+
+for file_name in $(ls -a $dotfiles_directory); do
+    # find dotfiles
+    if [[ $file_name == .* ]]; then
+        # ignore 'current dir' & 'parent dir'
+        if [[ "$file_name" != "." ]] && [[ $file_name != ".." ]]; then
+            # run the 'symlink_dotfile' function
+            symlink_dotfile $file_name
+        fi
+    fi
+done
 
 exit 0
