1ccc9be947b9678a4c7c1f86f690e9adceabf91c
skarre-r <pls.never@pm.me>
2022-08-04 23:12:33 +0200

    feat(SETUP.sh): add post-installation iterm config

--- SETUP.sh
+++ SETUP.sh
@@ -3,7 +3,6 @@
 # https://stackoverflow.com/a/20434740
 dotfiles_directory="$( cd "$( dirname "$0" )" && pwd )"
 
-
 symlink_dotfile() {
     file=$1
 
@@ -18,7 +17,6 @@ symlink_dotfile() {
     fi
 }
 
-
 for file_name in $(ls -a $dotfiles_directory); do
     # find dotfiles
     if [[ $file_name == .* ]]; then
@@ -29,5 +27,12 @@ for file_name in $(ls -a $dotfiles_directory); do
         fi
     fi
 done
+
+# iterm post-installation setup:
+# http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+if [[ -d "/Applications/iTerm.app" ]]; then
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.iterm"
+    defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
 
 exit 0
