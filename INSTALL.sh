f679410a0887266237d82f2f0161628c3cd4d81c
skarre-r <pls.never@pm.me>
2022-08-04 23:12:34 +0200

    feat(INSTALL.sh): update the script to install the 'spaceship' theme for oh-my-zsh

--- INSTALL.sh
+++ INSTALL.sh
@@ -14,12 +14,24 @@ fi
 echo
 
 # install oh-my-zsh: https://ohmyz.sh
-if [[ -d "$HOME/.oh-my-zsh" ]]; then
-    echo "oh-my-zsh is already installed"
-else
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     echo "oh-my-zsh is not installed"
     echo "Installing oh-my-zsh..."
     /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh is already installed"
+fi
+
+# install 'spaceship' theme for oh-my-zsh: https://spaceship-prompt.sh
+if [[ -d "$HOME/.oh-my-zsh" ]]; then
+    if [[ ! -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]]; then
+        echo "Spaceship theme is not install"
+        echo "Installing spaceship theme..."
+        git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+        ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+    else
+        echo "Spaceship theme is already installed"
+    fi
 fi
 
 exit 0
