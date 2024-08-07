74b5c3a06a19fca673241576c09e79babee05998
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-07 23:06:55 +0200

    chore: nvim: update plugin config(s)

--- nvim/init.lua
+++ nvim/init.lua
@@ -71,35 +71,110 @@ require('lazy').setup({
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        opts = {
-            ensure_installed = { "lua", "vim", "vimdoc" },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        }
-        -- config = function()
-        --     require("nvim-treesitter.configs").setup({
-        --         ensure_installed = { "lua", "vim", "vimdoc" },
-        --         sync_install = false,
-        --         auto_install = true,
-        --         highlight = { enable = true },
-        --         indent = { enable = true },
-        --     })
-        -- end
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "luadoc", "vim", "vimdoc", "markdown" },
+                sync_install = false,
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
     },
     {
         'neovim/nvim-lspconfig',
         dependencies = {
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/nvim-cmp',
+            'L3MON4D3/LuaSnip',         -- todo
+            'saadparwaiz1/cmp_luasnip', -- todo
             'j-hui/fidget.nvim'
         },
         config = function()
+            -- cmp = completion
+            local cmp = require("cmp")
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_lsp.default_capabilities())
+
             -- todo
+            require("fidget").setup({})
+
+            -- mason: installs lsps
             require("mason").setup()
-            require("mason-lspconfig").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls", "gopls"
+                },
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities
+                        }
+                    end,
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup {
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    runtime = { version = "Lua 5.4" },
+                                    diagnostics = {
+                                        globals = { "vim" }
+                                    }
+                                }
+                            }
+                        }
+                    end
+                }
+            })
+            -- todo what does this do
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_extend(args.body) -- luasnip
+                    end
+                },
+                mapping = cmp.mapping.preset.insert({ -- todo
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                })
+            })
+            vim.diagnostic.config({
+                -- update_in_insert = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            })
         end
+    },
+    {
+        'L3MON4D3/LuaSnip',
+        version = "v2.*",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        opts = {}
     },
     {
         'loctvl842/monokai-pro.nvim',
@@ -170,5 +245,51 @@ require('lazy').setup({
             -- "3rd/image.nvim",
         },
         opts = {}
+    },
+    { -- https://github.com/lewis6991/hover.nvim
+        "lewis6991/hover.nvim",
+        config = function()
+            require("hover").setup({
+                init = function()
+                    require("hover.providers.lsp")
+                end,
+                preview_opts = {
+                    border = "single"
+                },
+                preview_window = false,
+                title = true,
+                mouse_providers = { "LSP" },
+                mouse_delay = 500
+            })
+            -- Mouse support
+            vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
+            vim.o.mousemoveevent = true
+        end
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify", -- optional, fallback = 'mini'
+        },
+        opts = {
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = true,         -- use a classic bottom cmdline for search
+                command_palette = true,       -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false,       -- add a border to hover docs and signature help
+            },
+        },
     }
 })
