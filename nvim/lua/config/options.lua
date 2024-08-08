c19433e38763d70fdf09d742c11df35416163749
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-08 22:47:04 +0200

    chore: tweak nvim options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,6 +6,19 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.breakindent = true
 vim.opt.list = true -- show invisible characters
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.showmode = true
 vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+-- vim.opt.undofile = true
+-- vim.opt.updatetime = 250
+-- vim.opt.splitright = true
+-- vim.opt.splitbelow = true
+-- vim.opt.inccommand = 'split'
+-- vim.opt.hlsearch = true
