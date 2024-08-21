98431875897da7490a77a48508d2e9893c72b987
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-21 17:22:07 +0200

    nvim: add json and yaml lsp config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,28 @@
 -- NOTE: https://github.com/nvim-lualine/lualine.nvim
 -- TODO: extend config
 return {
-    "nvim-lualine/lualine.nvim",
-    lazy = false,
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = "auto", -- or "monokai-pro"
-        },
-    },
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"someone-stole-my-name/yaml-companion.nvim",
+	},
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto", -- or "monokai-pro"
+		},
+		sections = {
+			lualine_x = {
+				"fileformat",
+				"filetype", function()
+				local schema = require("yaml-companion").get_buf_schema(0)
+				if schema.result[1].name == "none" then
+					return ""
+				end
+				return schema.result[1].name
+			end
+			}
+		}
+	},
 }
