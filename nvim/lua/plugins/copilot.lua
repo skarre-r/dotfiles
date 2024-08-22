bb8e93f565b5f9bcd9739228c7301e4ef3dc2a3a
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-22 17:15:58 +0200

    nvim: tweak copilot settings

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,21 +4,43 @@
 return {
 	{
 		"zbirenbaum/copilot.lua",
-		cmd   = "Copilot",
+		cmd = "Copilot",
 		event = "InsertEnter",
-		opts  = {
+		keys = {
+			{
+				-- NOTE: https://github.com/zbirenbaum/copilot.lua/discussions/153
+				"<Tab>",
+				function()
+					if require("copilot.suggestion").is_visible() then
+						require("copilot.suggestion").accept()
+					else
+						vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+					end
+				end,
+				mode = "i",
+			},
+			-- {
+			-- 	"<Esc>",
+			-- 	function()
+			-- 		local pilot = require("copilot.suggestions")
+			-- 		if pilot.is_visible() then  end
+			-- 	end,
+			-- 	mode = "i",
+			-- },
+		},
+		opts = {
 			panel = { enabled = false },
 			suggestion = {
 				enabled = true,
 				auto_trigger = true,
 				keymap = {
 					--accept = "<Tab>"
-				}
-			}
+				},
+			},
 		},
 	},
-	{
-		"zbirenbaum/copilot-cmp",
-		enabled = false,
-	}
+	-- {
+	-- 	"zbirenbaum/copilot-cmp",
+	-- 	enabled = false,
+	-- },
 }
