914e1f51efcb01dffac6ca5debea80d54aea6acb
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-22 17:17:40 +0200

    nvim: ensure more linters/ formatters are installed

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,33 +3,41 @@
 -- https://github.com/williamboman/mason-lspconfig.nvim
 -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
 return {
-    {
-        "williamboman/mason.nvim",
-        cmd = "Mason",
-        build = ":MasonUpdate",
-        event = "VeryLazy",
-        opts = {}
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        event = "VeryLazy",
-        opts = {}
-    },
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        event = "VeryLazy",
-        opts = {
-            ensure_installed = {
-                "ruff",
-                "biome",
-                "stylua",
-                "prettier",
-                "eslint_d",
-                "goimports",
-                "golangci-lint",
-            },
-            auto_update = true,
-            run_on_start = true,
-        }
-    }
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		build = ":MasonUpdate",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		event = "VeryLazy",
+		opts = {
+			ensure_installed = {
+				"ruff",
+				"biome",
+				"stylua",
+				"prettier",
+				"prettierd",
+				"eslint_d",
+				"goimports",
+				"golangci-lint",
+				"commitlint",
+				"jsonlint",
+				"luacheck",
+				"markdownlint",
+				"shellcheck",
+				"yamllint",
+				"goimports",
+			},
+			auto_update = true,
+			run_on_start = true,
+		},
+	},
 }
