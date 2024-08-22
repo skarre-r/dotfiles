44899bd67f6daf5272b665a3d72f970b3aac6ac9
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-22 17:15:29 +0200

    nvim: add keymap to trigger formatter(s) manually

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,39 +1,49 @@
 -- DOCS:
 -- https://github.com/stevearc/conform.nvim
 return {
-    "stevearc/conform.nvim",
-    cmd = "ConformInfo",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "ruff" },
-            css = { "prettier" },
-            html = { "prettier" },
-            json = { "prettier" },
-            yaml = { "prettier" },
-            markdown = { "prettier" },
-            graphql = { "prettier" },
-            javascript = { "biome", "prettier", stop_after_first = true },
-            typescript = { "biome", "prettier", stop_after_first = true },
-            javascriptreact = { "biome", "prettier", stop_after_first = true },
-            typescriptreact = { "biome", "prettier", stop_after_first = true },
-            go = { "goimports", "gofmt" },
-        },
-        default_format_opts = {
-            timeout_ms = 3000,
-            async = false,
-            quiet = false,
-            lsp_format = "fallback",
-        },
-        format_on_save = {
-            lsp_format = "fallback",
-            timeout_ms = 500,
-        },
-        format_after_save = {
-            lsp_format = "fallback"
-        },
-        notify_on_error = true,
-        formatters = {}, -- NOTE: custom formatters
-    },
+	"stevearc/conform.nvim",
+	cmd = "ConformInfo",
+	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "n",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
+			javascript = { "biome", "prettier", stop_after_first = true },
+			typescript = { "biome", "prettier", stop_after_first = true },
+			javascriptreact = { "biome", "prettier", stop_after_first = true },
+			typescriptreact = { "biome", "prettier", stop_after_first = true },
+			go = { "goimports", "gofmt" },
+			sql = {},
+		},
+		default_format_opts = {
+			timeout_ms = 3000,
+			async = false,
+			quiet = false,
+			lsp_format = "fallback",
+		},
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
+		},
+		format_after_save = {
+			lsp_format = "fallback",
+		},
+		notify_on_error = true,
+		formatters = {}, -- NOTE: custom formatters
+	},
 }
