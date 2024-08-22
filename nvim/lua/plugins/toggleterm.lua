1a51d0f364828afa2622577f36c678b66953e37d
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-22 17:13:30 +0200

    nvim: toggle terminal with Shift+CMD+j

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,20 +1,21 @@
 -- DOCS:
 -- https://github.com/akinsho/toggleterm.nvim
 return {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    keys = {
-        -- NOTE: toggle terminal w/ CMD+j
-        { "<D-j>", "<CMD>ToggleTerm<CR>", mode = { "n", "v", "i", "t" } }
-    },
-    opts = {
-        size = 25,
-        direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float',
-        float_opts = {
-            border = "curved",
-        },
-        auto_scroll = true,
-        start_in_insert = true,
-        autochdir = false,
-    },
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	keys = {
+		-- NOTE: toggle terminal w/ CMD+j
+		{ "<D-j>", "<CMD>ToggleTerm<CR>", mode = { "n", "v", "i", "t" } },
+		{ "<S-D-j>", "<CMD>ToggleTerm<CR>", mode = { "n", "v", "i", "t" } },
+	},
+	opts = {
+		size = 25,
+		direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float',
+		float_opts = {
+			border = "curved",
+		},
+		auto_scroll = true,
+		start_in_insert = true,
+		autochdir = false,
+	},
 }
