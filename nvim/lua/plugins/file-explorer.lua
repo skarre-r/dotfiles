34ad8b66c579441a534960c86949f4eb67fb7854
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-08-23 16:38:55 +0200

    chore: nvim: rename the oil plugin file

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -1,23 +1,27 @@
+-- TODO: nvim-tree, neo-tree, oil, mini.files()
 -- DOCS:
 -- https://github.com/stevearc/oil.nvim
 return {
 	"stevearc/oil.nvim",
-	-- dependencies = { "nvim-tree/nvim-web-devicons" },
-	depedencies = {
-		{ "echasnovski/mini.icons", opts = {} },
-	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
 		{ "<S-D-o>", "<CMD>Oil --float<CR>", mode = "n" },
 	},
 	opts = {
 		default_file_explorer = false,
-		colums = { "icon" },
+		columns = { "icon" },
+		win_options = {
+			wrap = false,
+		},
 		delete_to_trash = true,
+		constrain_cursor = "editable",
 		view_options = {
 			show_hidden = true,
 		},
 		float = {
 			padding = 10,
+			max_width = 200,
+			border = "rounded",
 		},
 	},
 }
