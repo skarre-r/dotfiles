2d0b949b63e06acc4771978d41fb0ff57817e8c0
skarre-r <59067313+skarre-r@users.noreply.github.com>
2024-09-05 16:19:10 +0200

    chore: turn off lazy.nvim update notifications

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,35 +1,35 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable", -- latest stable release
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable", -- latest stable release
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = { import = "plugins" },
-    defaults = { lazy = true },
-    install = { missing = true },
-    checker = { enabled = true, notify = true, frequency = 3600 },
-    change_detection = { enabled = true, notify = false },
-    performance = {
-        cache = { enabled = true },
-        reset_packpath = true,
-        rtp = {
-            reset = true,
-            disabled_plugins = {
-                "gzip",
-                "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
+	spec = { import = "plugins" },
+	defaults = { lazy = true },
+	install = { missing = true },
+	checker = { enabled = true, notify = false, frequency = 3600 },
+	change_detection = { enabled = true, notify = false },
+	performance = {
+		cache = { enabled = true },
+		reset_packpath = true,
+		rtp = {
+			reset = true,
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
