677f4246a5ae8299ebc6db10b7e7551a1fa0b8f1
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-09 16:34:18 +0100

    nix: enable deamon

--- nix/flake.nix
+++ nix/flake.nix
@@ -12,15 +12,13 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
+      services.nix-daemon.enable = true;
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =[ pkgs.vim ];
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
