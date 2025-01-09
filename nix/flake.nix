4f307d2564eceaa08b3f5e2a4eb59267828fc3e2
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-10 00:05:53 +0100

    nix: update darwin config; add initial home-manager config

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,3 +1,6 @@
+# nix-darwin options: https://mynixos.com/nix-darwin/options
+# home-manager options: https://mynixos.com/home-manager/options
+
 {
   description = "nix-darwin system flake";
 
@@ -15,36 +18,90 @@
 
   outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, ... }:
   let
-    configuration = { pkgs, ... }: {
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # Enable nix deamon
-      services.nix-daemon.enable = true;
-
-      # Packages
-      environment.systemPackages =[
+    darwinConfiguration = { pkgs, ... }: {
+      environment.systemPackages = [
         pkgs.vim
         pkgs.nixd
         pkgs.nil
       ];
+
+      homebrew = {
+        enable = true;
+        taps = [];
+        brews = [];
+        casks = [];
+        masApps = {};
+        onActivation = {
+          autoUpdate = false;
+          cleanup = "none";  # TODO: "uninstall" or "zap"
+          upgrade = false;
+        };
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      programs = {
+        zsh = {
+          enable = true;
+          enableBashCompletion = true;
+          enableCompletion = true;
+          enableFastSyntaxHighlighting = true;
+          enableFzfCompletion = true;
+          promptInit = "eval $(starship init zsh)";  # TODO
+        };
+        fish = {
+          enable = true;
+          promptInit = "starship init fish | source";  # TODO
+        };
+      };
+
+      services = {
+        nix-daemon.enable = true;
+      };
+
+      # Enable touch id sudo
+      security.pam.enableSudoTouchIdAuth = true;
+
+      system = {
+        defaults = {};  # TODO
+        # Set Git commit hash for darwin-version.
+        configurationRevision = self.rev or self.dirtyRev or null;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        stateVersion = 5;
+      };
+
+      users.users.skar = {
+        name = "skar";
+        home = "/Users/skar";
+      };
+    };
+
+    homeManagerConfiguration = { pkgs, ... }: {
+      home.stateVersion = "24.11";
+      programs.home-manager.enable = true;
+      home.packages = with pkgs; [];
     };
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    # $ darwin-rebuild build --flake /path/to/nix#home
+    darwinConfigurations = {
+      "home" = nix-darwin.lib.darwinSystem {
+        modules = [
+          darwinConfiguration
+          home-manager.darwinModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users.skar = homeManagerConfiguration;
+          }
+        ];
+      };
     };
   };
 }
