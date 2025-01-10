c7ea4f98cb2a8050038c81094fe58526c338d61e
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-10 09:58:31 +0100

    fish: enable 'transient' starship promot

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,16 @@
 set -U fish_greeting
 
 # init starship
+function starship_transient_prompt_func
+  starship module character
+end
+
+function starship_transient_rprompt_func
+  starship module time
+end
+
 starship init fish | source
+enable_transience
 
 # init fzf
 fzf --fish | source
