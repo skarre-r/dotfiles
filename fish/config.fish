b94bbadd9d7f4d496260b6aec585cd78f8432768
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-10 09:44:30 +0100

    fish: remove the welcome message

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,14 @@
 
+# remove the welcom message
+set -U fish_greeting
+
+# init starship
 starship init fish | source
 
+# init fzf
 fzf --fish | source
 
+# source shell aliases
 if test -e $HOME/.aliases
     source $HOME/.aliases
 end
