0cf9ed471bf5ac76b237c0d30c610e19dc2952df
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-14 13:09:36 +0100

    nix: update 'system.defaults' (macOS settings) in the base module; move some settings to flake.nix

--- flake.nix
+++ flake.nix
@@ -31,7 +31,15 @@
         # nixpkgs
         {
           nixpkgs.hostPlatform = "aarch64-darwin";
+          # Allow "unfree" packages.
           nixpkgs.config.allowUnfree = true;
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
         }
         # home-manager
         home-manager.darwinModules.home-manager
--- nix/base.nix
+++ nix/base.nix
@@ -121,9 +121,6 @@
   homebrew.onActivation.autoUpdate = true;
   homebrew.onActivation.upgrade = false;
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
-
   programs.fish.enable = true;
   programs.zsh = {
     enable = true;
@@ -148,13 +145,112 @@
   # Enable touch id sudo
   security.pam.enableSudoTouchIdAuth = true;
 
-  system = {
-    defaults = { }; # TODO
-    # Set Git commit hash for darwin-version.
-    configurationRevision = config.rev or config.dirtyRev or null;
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    stateVersion = 5;
+  # macOS settings
+  system.defaults = {
+    ActivityMonitor = {
+      IconType = 1;
+      OpenMainWindow = true;
+      ShowCategory = 100;
+    };
+    controlcenter = {
+      BatteryShowPercentage = true;
+      Sound = true;
+    };
+    dock = {
+      appswitcher-all-displays = false;
+      autohide = true;
+      autohide-delay = null;
+      expose-animation-duration = 0.1;
+      expose-group-apps = true;
+      launchanim = false;
+      magnification = false;
+      mineffect = "scale";
+      minimize-to-application = true;
+      mouse-over-hilite-stack = true;
+      persistent-apps = null; # TODO
+      persistent-others = null;
+      show-process-indicators = true;
+      show-recents = false;
+      showhidden = true;
+      slow-motion-allowed = false;
+      static-only = false;
+      tilesize = 48;
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
+    };
+    finder = {
+      _FXShowPosixPathInTitle = true;
+      _FXSortFoldersFirst = true;
+      _FXSortFoldersFirstOnDesktop = true;
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      FXEnableExtensionChangeWarning = false;
+      FXPreferredViewStyle = "Nlsv";
+      FXRemoveOldTrashItems = false;
+      NewWindowTarget = "Other";
+      NewWindowTargetPath = "file:///Users/skar/Downloads";
+      QuitMenuItem = true;
+      ShowExternalHardDrivesOnDesktop = false;
+      ShowHardDrivesOnDesktop = false;
+      ShowMountedServersOnDesktop = false;
+      ShowPathbar = true;
+      ShowRemovableMediaOnDesktop = false;
+      ShowStatusBar = true;
+    };
+    menuExtraClock = {
+      FlashDateSeparators = false;
+      IsAnalog = false;
+      Show24Hour = true;
+      ShowAMPM = false;
+      ShowDate = 0;
+      ShowDayOfMonth = true;
+      ShowDayOfWeek = true;
+      ShowSeconds = true;
+    };
+    NSGlobalDomain = {
+      "com.apple.keyboard.fnState" = false;
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.sound.beep.feedback" = 1;
+      "com.apple.springing.enabled" = true;
+      "com.apple.swipescrolldirection" = true;
+      "com.apple.trackpad.enableSecondaryClick" = true;
+      _HIHideMenuBar = false; # autohide menu bar
+      AppleFontSmoothing = null;
+      AppleICUForce24HourTime = true;
+      AppleInterfaceStyle = "Dark";
+      AppleInterfaceStyleSwitchesAutomatically = false;
+      AppleScrollerPagingBehavior = true;
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      AppleShowScrollBars = "Automatic";
+      AppleSpacesSwitchOnActivate = true;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticInlinePredictionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSAutomaticWindowAnimationsEnabled = false;
+      NSScrollAnimationEnabled = true;
+      NSTableViewDefaultSizeMode = 2;
+    };
+    screencapture = {
+      disable-shadow = false;
+      include-date = true;
+      location = "/Users/skar/Pictures";
+    };
+    spaces.spans-displays = false;
+    trackpad = {
+      ActuationStrength = 1;
+      Clicking = true;
+      Dragging = true;
+      TrackpadRightClick = true;
+      TrackpadThreeFingerDrag = false;
+      TrackpadThreeFingerTapGesture = 0;
+    };
+    universalaccess.reduceMotion = false;
   };
 
   users.knownUsers = [ "skar" ];
