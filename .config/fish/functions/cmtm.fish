802abb92c09c976256480b80a80617c90ebe045a
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-15 13:50:41 +0100

    chore(fish): fix the 'cmtm' function

--- .config/fish/functions/cmtm.fish
+++ .config/fish/functions/cmtm.fish
@@ -1,6 +1,14 @@
 
 function cmtm
-    if test -d .git && $argv[1] != "" && $argv[2] != ""
-        git commit -m $argv[1] -m $argv[2]
+    if git rev-parse --is-inside-work-tree &> /dev/null
+        if not test -d $argv[1]
+            if not test -d $argv[2]
+                git commit -m $argv[1] -m $argv[2]
+            else
+                echo "cmtm: 2 arguments required"
+            end
+        else
+            echo "cmtm: 2 arguments required"
+        end
     end
 end
