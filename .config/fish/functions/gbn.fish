952a72be3aaa20efabeffd2f75ef1fc63982e16d
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-15 13:50:26 +0100

    chore(fish): fix the 'gbn' function

--- .config/fish/functions/gbn.fish
+++ .config/fish/functions/gbn.fish
@@ -1,14 +1,18 @@
 
+# TODO: stash changes > switch to branch > push branch > pop stash
 function gbn
-    if test -d .git
-        if $argv[1] != ""
-            if $argv[2] != ""
-                remote=$argv[2]
+    if git rev-parse --is-inside-work-tree &> /dev/null
+        if test -d $argv[1]
+            echo "branch name required"
+        else
+            if test -d $argv[2]
+                set -f remote "origin"
             else
-                remote="origin"
+                set -f remote $argv[2]
             end
-            git switch --create $argv[1]
-            git push -u $remote $argv[1]
+            if git switch --create $argv[1]
+                git push -u $remote $argv[1]
+            end
         end
     end
 end
