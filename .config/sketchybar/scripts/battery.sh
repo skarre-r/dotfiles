8a570400d58bad09850eaaad0d2aca9c42f08d6b
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-16 09:57:25 +0100

    sketchybar: change the battery icon when charging

--- .config/sketchybar/scripts/battery.sh
+++ .config/sketchybar/scripts/battery.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-sketchybar --set $NAME label="$(pmset -g batt | grep -Eo '[[:digit:]]+%' | cut -d% -f1)%"
+AC_POWER=$(pmset -g batt | grep 'AC Power')
+
+if [[ $AC_POWER != "" ]]; then
+    ICON="􀋦"
+else
+    ICON="􀺶"
+fi
+
+sketchybar --set $NAME label="$(pmset -g batt | grep -Eo '[[:digit:]]+%' | cut -d% -f1)%" icon="$ICON"
