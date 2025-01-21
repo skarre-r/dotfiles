be03597430fae1e0d89a9f491f07bf39d049d289
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-21 13:29:38 +0100

    chore(fish): add 'goland' and 'pycharm' functions

---
+++ .config/fish/functions/goland.fish
@@ -0,0 +1,6 @@
+
+function goland
+    if test -e /Applications/GoLand.app/Contents/MacOS/goland
+        /Applications/GoLand.app/Contents/MacOS/goland $argv
+    end
+end
---
+++ .config/fish/functions/pycharm.fish
@@ -0,0 +1,6 @@
+
+function pycharm
+    if test -e /Applications/PyCharm.app/Contents/MacOS/pycharm
+        /Applications/PyCharm.app/Contents/MacOS/pycharm $argv
+    end
+end
