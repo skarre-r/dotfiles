a067bc4eee989ede005bb5bdc93074ee7ff51daa
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-01-21 10:06:53 +0100

    chore(fish): update the 'gbn' function to stash changes before switching branch

--- .config/fish/functions/gbn.fish
+++ .config/fish/functions/gbn.fish
@@ -1,5 +1,4 @@
 
-# TODO: stash changes > switch to branch > push branch > pop stash
 function gbn
     if git rev-parse --is-inside-work-tree &> /dev/null
         if test -d $argv[1]
@@ -10,8 +9,20 @@ function gbn
             else
                 set -f remote $argv[2]
             end
-            if git switch --create $argv[1]
-                git push -u $remote $argv[1]
+            # stash changes
+            if test -n "$(git status --porcelain)"
+                set -f dirty 1
+                git stash save --include-untracked
+            else
+                set -f dirty 0
+            end
+            # create new branch
+            git switch --create $argv[1]
+            # push branch to remote
+            git push -u $remote $argv[1]
+            # pop changes
+            if test $dirty -eq 1
+                git stash pop
             end
         end
     end
