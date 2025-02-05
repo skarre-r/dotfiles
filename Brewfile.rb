5a185ed8913cb49d016cadc11dd834b57dd55cec
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-02-05 22:37:30 +0100

    chore: add brewfile to keep track of unused homebrew casks

---
+++ Brewfile.rb
@@ -0,0 +1,12 @@
+# This file isn't actually used for anything
+# except for keeping track of unused homebrew casks, etc.
+
+cask "yaak"
+cask "httpie"
+cask "openemu"
+cask "itsycal"
+cask "raindropio"
+cask "balenaetcher"
+cask "monitorcontrol"
+cask "keyboard-cowboy"
+cask "reminders-menubar"
