6bbc443758b27311a03bf100184024b8522911d0
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-02-06 13:17:40 +0100

    chore(nix): fix cache settings in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,24 +4,6 @@
 {
   description = "nix-darwin system flake";
 
-  nixConfig = {
-    substituters = [
-      "https://cache.nixos.org"
-    ];
-    trusted-substituters = [
-      "https://cache.nixos.org"
-      "https://cachix.cachix.org"
-      "https://nixpkgs.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-stable.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
@@ -48,11 +30,34 @@
       sharedModules = [
         # nix(pkgs) settings
         {
-          nixpkgs.hostPlatform = "aarch64-darwin";
-          # Allow "unfree" packages.
-          nixpkgs.config.allowUnfree = true;
+          # Auto optimise?
+          nix.optimise.automatic = true;
+          # Cache settings
+          nix.settings.substituters = [ "https://cache.nixos.org" ];
+          nix.settings.trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+            "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+            "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+          nix.settings.trusted-substituters = [
+            "https://cache.nixos.org"
+            "https://hydra.nixos.org/"
+            "https://cachix.cachix.org"
+            "https://nixpkgs.cachix.org"
+            "https://nix-community.cachix.org"
+          ];
+          nix.settings.trusted-users = [
+            "skar"
+          ];
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
+          # Architecture
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          nixpkgs.buildPlatform = "aarch64-darwin";
+          # Allow "unfree" packages.
+          nixpkgs.config.allowUnfree = true;
           # Set Git commit hash for darwin-version.
           system.configurationRevision = self.rev or self.dirtyRev or null;
           # Used for backwards compatibility, please read the changelog before changing.
@@ -81,6 +86,8 @@
         inherit inputs;
         pkgs-stable = import nix-stable {
           system = "aarch64-darwin";
+          hostPlatform = "aarch64-darwin";
+          buildPlatform = "aarch64-darwin";
           config.allowUnfree = true;
         };
       };
