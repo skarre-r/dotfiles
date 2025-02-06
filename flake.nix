c220f249013bb499d22ae3d3d39448bc4af8a663
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-02-06 10:48:19 +0100

    chore(nix): update flake.nix cache settings

--- flake.nix
+++ flake.nix
@@ -4,6 +4,24 @@
 {
   description = "nix-darwin system flake";
 
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+    ];
+    trusted-substituters = [
+      "https://cache.nixos.org"
+      "https://cachix.cachix.org"
+      "https://nixpkgs.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-stable.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
