a7ee2f51d012a3ea2fd26220aff8ad9f332b8fd0
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-02-07 10:56:11 +0100

    chore: update brewfile

--- Brewfile.rb
+++ Brewfile.rb
@@ -2,9 +2,13 @@
 # except for keeping track of unused homebrew casks, etc.
 
 cask "yaak"
+cask "lasso"
+cask "fleet"
 cask "httpie"
+cask "typora"
 cask "openemu"
 cask "itsycal"
+cask "wireshark"
 cask "raindropio"
 cask "balenaetcher"
 cask "monitorcontrol"
