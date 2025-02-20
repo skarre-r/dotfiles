ad613f3d1f98827cf5bf22085d08ddf51b2363f2
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-02-20 13:04:34 +0100

    chore(fish homebrew function): add todos

--- .config/fish/functions/homebrew.fish
+++ .config/fish/functions/homebrew.fish
@@ -1,3 +1,9 @@
+
+# TODO:
+# - check if "brew" command exists
+# - get path using "brew --prefix"
+# - check if the command actually exists
+
 function homebrew
     /opt/homebrew/bin/$argv[1] $argv[2..-1]
 end
