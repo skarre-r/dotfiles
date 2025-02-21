b5d26ff984f0ba7b58731f2d1018b09ab1736afa
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-02-21 10:35:50 +0100

    chore: improve the 'homebrew' fish function
    - check if brew is installed
    - check if the specified binary actually exists

--- .config/fish/functions/homebrew.fish
+++ .config/fish/functions/homebrew.fish
@@ -1,9 +1,15 @@
 
-# TODO:
-# - check if "brew" command exists
-# - get path using "brew --prefix"
-# - check if the command actually exists
-
 function homebrew
-    /opt/homebrew/bin/$argv[1] $argv[2..-1]
+    if command -v brew > /dev/null
+        set -f brew_path (brew --prefix)
+        if test -e "$brew_path/bin/$argv[1]"
+            /opt/homebrew/bin/$argv[1] $argv[2..-1]
+        else
+            echo "command not found"
+            return 1
+        end
+    else
+        echo "brew is not installed"
+        return 1
+    end
 end
