07eb87787e0d2e74cb74e0a657c419d6339243e7
skarre-r <59067313+skarre-r@users.noreply.github.com>
2025-04-11 16:01:56 +0200

    chore(fish): combine github pull request functions

--- .config/fish/functions/github-pull-requests.fish
+++ .config/fish/functions/github-pull-requests.fish
@@ -1,4 +1,5 @@
-function github-pr-close
+
+function github-pull-requests -a action
     if ! command -v gh > /dev/null
         echo "gh is not installed!"
         return 1
@@ -12,13 +13,36 @@ function github-pr-close
         return 1
     end
 
+    if not contains $action "open" "close" "merge" "squash"
+        echo "github-pull-requests [open|close|merge|squash]"
+        return 1
+    end
+
     set -f prs (gh pr list --json number,title,headRefName,labels --template '{{range .}}{{tablerow .number .title .headRefName}}{{end}}' | fzf --reverse --multi --accept-nth=1)
-    if test -n "$prs"
+    if not test -n "$prs"
+        return 1
+    end
+
+    switch $action
+    case "open"
+        for pr in $prs
+            gh pr view "$pr" --web
+        end
+        return 0
+    case "close"
         for pr in $prs
             gh pr close "$pr"
         end
         return 0
-    else
-        return 1
+    case "merge"
+        for pr in $prs
+            gh pr merge "$pr" --merge
+        end
+        return 0
+    case "squash"
+        for pr in $prs
+            gh pr merge "$pr" --squash
+        end
+        return 0
     end
 end
